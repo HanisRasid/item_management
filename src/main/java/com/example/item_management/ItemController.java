8e824e890f1abea714de2ca7e805ff81b37d2e04
Hanis <s3781755@student.rmit.edu.au>
2022-09-08 23:28:08 +1000

    sets up Item class and ItemDao

--- src/main/java/com/example/item_management/ItemController.java
+++ src/main/java/com/example/item_management/ItemController.java
@@ -1,4 +1,8 @@
 package com.example.item_management;
 
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
 public class ItemController {
 }
--- src/main/java/com/example/item_management/ItemDao.java
+++ src/main/java/com/example/item_management/ItemDao.java
@@ -1,4 +1,28 @@
 package com.example.item_management;
 
+import java.util.*;
+
 public class ItemDao {
+    private static Map<String, Item> items = new HashMap<String, Item>();
+
+    static{
+        Item item1 = new Item("01", "item1", "desc for item1", 1.0);
+        Item item2 = new Item("02", "item2", "desc for item2", 2.0);
+        items.put("01", item1);
+        items.put("02", item2);
+    }
+    public static List<Item> getAllItems(){
+        return new ArrayList<>(items.values());
+    }
+    public static Item getItemDetails(double itemId){
+        return items.get(itemId);
+    }
+    public static Item updateItem(String itemId, Item item){
+        item.setId(itemId);
+        items.put(itemId,item);
+        return item;
+    }
+    public static Item deleteItem(double itemId){
+        return items.remove(itemId);
+    }
 }
